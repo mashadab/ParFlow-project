e3464947a49a79ce6b07923d94c3cf1163d66b32
Mohammad Afzal Shadab <ms6985@verde.princeton.edu>
2024-09-17 10:07:00 -0400

    Richards plots

--- Richards_eq_analysis.py
+++ Richards_eq_analysis.py
@@ -9,6 +9,7 @@ Date: 9 Sept, 2024
 #import libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 #Parameters from Celia
 Ks = 0.00944 #cm/s
@@ -157,6 +158,7 @@ plt.savefig(f'theta_vs_head_vG.pdf',bbox_inches='tight', dpi = 600)
 alpha_vG = 1.0
 h = np.linspace(-10,0,10000)
 s_s = 1.0; s_r = 0.2
+sat_arr = np.linspace(s_r,s_s,10000)
 
 #Van Genuchten
 sw_vG= lambda h,n: (s_s - s_r)/((1 + np.abs(alpha_vG*h)**n)**(1-1/n))+s_r #sw, head in cms
@@ -166,6 +168,12 @@ kr_vG= lambda h,n: (1-(np.abs(alpha_vG*h)**(n-1))/(1+np.abs(alpha_vG*h)**n)**(1-
 sw_haverkamp= lambda h,gamma: A*(s_s - s_r)/(A + np.abs(h)**gamma)+s_r #sw, head in cms
 kr_haverkamp= lambda h,gamma: A/(A + np.abs(h)**gamma) #kr, head in cms
 
+#Ideal/kinematic
+sw_vG_ideal= lambda h: s_s*(h>=0)+s_r*(h<0) #sw, head in cms
+kr_vG_ideal= lambda h: 1.0*(h>=0)+0.0*(h<0)
+se_vG= lambda sw: ((sw - s_r)/(s_s - s_r))
+kr_vG_ideal_sat = lambda sw,n: se_vG(sw)**(1/2)*(1-(1-se_vG(sw)**(1/(1-1/n)))**(1-1/n))**2
+kr_haverkamp_ideal_sat = lambda sw: se_vG(sw)
 
 fig = plt.figure(figsize=(7.5,5) , dpi=100)
 plt.plot(sw_vG(h,1),h,'-',color='k',alpha=0.2,label=r'$n=1$')
@@ -173,6 +181,7 @@ plt.plot(sw_vG(h,2),h,'-',color='k',alpha=0.4,label=r'$n=2$')
 plt.plot(sw_vG(h,10),h,'-',color='k',alpha=0.6,label=r'$n=10$')
 plt.plot(sw_vG(h,50),h,'-',color='k',alpha=0.8,label=r'$n=50$')
 plt.plot(sw_vG(h,100),h,'-',color='k',alpha=1.0,label=r'$n=100$')
+plt.plot(sw_vG_ideal(h),h,'--',color='r',alpha=1.0,label=r'kinematic')
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$h$ [cm]')
@@ -189,6 +198,7 @@ plt.plot(kr_vG(h,2),h,'-',color='k',alpha=0.4,label=r'$n=2$')
 plt.plot(kr_vG(h,10),h,'-',color='k',alpha=0.6,label=r'$n=10$')
 plt.plot(kr_vG(h,50),h,'-',color='k',alpha=0.8,label=r'$n=50$')
 plt.plot(kr_vG(h,100),h,'-',color='k',alpha=1.0,label=r'$n=100$')
+plt.plot(kr_vG_ideal(h),h,'--',color='r',alpha=1.0,label=r'kinematic')
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$h$ [cm]')
@@ -206,6 +216,7 @@ plt.plot(sw_haverkamp(h,2),h,'-',color='k',alpha=0.4,label=r'$\gamma=2$')
 plt.plot(sw_haverkamp(h,10),h,'-',color='k',alpha=0.6,label=r'$\gamma=10$')
 plt.plot(sw_haverkamp(h,50),h,'-',color='k',alpha=0.8,label=r'$\gamma=50$')
 plt.plot(sw_haverkamp(h,100),h,'-',color='k',alpha=1.0,label=r'$\gamma=100$')
+plt.plot(sw_vG_ideal(h),h,'--',color='r',alpha=1.0,label=r'kinematic')
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$h$ [cm]')
@@ -222,6 +233,7 @@ plt.plot(kr_haverkamp(h,2),h,'-',color='k',alpha=0.4,label=r'$\gamma=2$')
 plt.plot(kr_haverkamp(h,10),h,'-',color='k',alpha=0.6,label=r'$\gamma=10$')
 plt.plot(kr_haverkamp(h,50),h,'-',color='k',alpha=0.8,label=r'$\gamma=50$')
 plt.plot(kr_haverkamp(h,100),h,'-',color='k',alpha=1.0,label=r'$\gamma=100$')
+plt.plot(kr_vG_ideal(h),h,'--',color='r',alpha=1.0,label=r'kinematic')
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$h$ [cm]')
@@ -233,12 +245,17 @@ plt.legend()
 plt.savefig(f'kr_vs_head_haverkamp.pdf',bbox_inches='tight', dpi = 600)
 
 
+kr_vG_ideal_sat = lambda sw,n: se_vG(sw)**(1/2)*(1-(1-se_vG(sw)**(1/(1-1/n)))**(1-1/n))**2
+kr_haverkamp_ideal_sat = lambda sw: se_vG(sw)
+
+
 fig = plt.figure(figsize=(7.5,5) , dpi=100)
 plt.plot(sw_haverkamp(h,1),kr_haverkamp(h,1),'-',color='k',alpha=0.2,label=r'$\gamma=1$')
 plt.plot(sw_haverkamp(h,2),kr_haverkamp(h,2),'-',color='k',alpha=0.4,label=r'$\gamma=2$')
 plt.plot(sw_haverkamp(h,10),kr_haverkamp(h,10),'-',color='k',alpha=0.6,label=r'$\gamma=10$')
 plt.plot(sw_haverkamp(h,50),kr_haverkamp(h,50),'-',color='k',alpha=0.8,label=r'$\gamma=50$')
 plt.plot(sw_haverkamp(h,100),kr_haverkamp(h,100),'-',color='k',alpha=1.0,label=r'$\gamma=100$')
+plt.plot(sat_arr,kr_haverkamp_ideal_sat(sat_arr),'--',color='r',alpha=1.0,label=r'kinematic')
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$k_r(h)$')
@@ -251,11 +268,16 @@ plt.savefig(f's_w_vs_kr_haverkamp.pdf',bbox_inches='tight', dpi = 600)
 
 
 fig = plt.figure(figsize=(7.5,5) , dpi=100)
-plt.plot(sw_vG(h,1),kr_vG(h,1),'-',color='k',alpha=0.2,label=r'$n=1$')
+#plt.plot(sw_vG(h,1),kr_vG(h,1),'-',color='k',alpha=0.2,label=r'$n=1$')
 plt.plot(sw_vG(h,2),kr_vG(h,2),'-',color='k',alpha=0.4,label=r'$n=2$')
 plt.plot(sw_vG(h,10),kr_vG(h,10),'-',color='k',alpha=0.6,label=r'$n=10$')
 plt.plot(sw_vG(h,50),kr_vG(h,50),'-',color='k',alpha=0.8,label=r'$n=50$')
 plt.plot(sw_vG(h,100),kr_vG(h,100),'-',color='k',alpha=1.0,label=r'$n=100$')
+#plt.plot(sat_arr,kr_vG_ideal_sat(sat_arr,1),'--',color='r',alpha=0.2,label=r'kinematic')
+plt.plot(sat_arr,kr_vG_ideal_sat(sat_arr,2),'--',color='r',alpha=0.4)
+plt.plot(sat_arr,kr_vG_ideal_sat(sat_arr,10),'--',color='r',alpha=0.6)
+plt.plot(sat_arr,kr_vG_ideal_sat(sat_arr,50),'--',color='r',alpha=0.8)
+plt.plot(sat_arr,kr_vG_ideal_sat(sat_arr,100),'--',color='r',alpha=1.0)
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 plt.ylabel(r'$k_r(h)$')
@@ -265,3 +287,14 @@ plt.xlabel(r'$s_w(h)$')
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 plt.legend()
 plt.savefig(f's_w_vs_kr_vG.pdf',bbox_inches='tight', dpi = 600)
+
+
+N_saturation = 100.0; N_RelPerm = 100.0
+sw_vG= lambda h,n: (s_s - s_r)/((1 + np.abs(alpha_vG*h)**n)**(1-1/n))+s_r
+sw_vG_invert= lambda h: sw_vG(h,N_saturation)-1.0
+x = fsolve(sw_vG_invert,0.5)
+
+kr_vG= lambda h,n: (1-(np.abs(alpha_vG*h)**(n-1))/(1+np.abs(alpha_vG*h)**n)**(1-1/n))**2.0/((1 + np.abs(alpha_vG*h)**n)**((1-1/n)/2)) #kr, head in cms
+print('kr=',kr_vG(x,N_RelPerm))
+
+print('head',x,' m')
